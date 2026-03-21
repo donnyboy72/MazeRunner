d2038251099f8d7a42299384f3ee849f4ec6a1ff
Donavan Watley <76824069+donnyboy72@users.noreply.github.com>
2026-03-21 19:20:44 -0400

    Finished DFS and added player
    There is still no logic for the player. At the moment you can only move around.

--- src/MazeRunner.java
+++ src/MazeRunner.java
@@ -10,6 +10,10 @@ import javax.swing.Timer;
 public class MazeRunner extends JPanel implements ActionListener, KeyListener
 {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	//graphics set up
 	int windowW; //width of window
 	int windowH; //height of window
@@ -18,9 +22,15 @@ public class MazeRunner extends JPanel implements ActionListener, KeyListener
 	//maze set up
 	int mazeWidth;
 	int mazeHeight;
+	int start;
+	int end;
 	Tile[][] maze;
 	
-	//Prims set up
+	//Player set up
+	int playerX;
+	int playerY;
+	int velocityX;
+	int velocityY;
 	
 	Timer gameLoop;
 	Random random = new Random();
@@ -35,10 +45,25 @@ public class MazeRunner extends JPanel implements ActionListener, KeyListener
 		this.windowH = height;
 		setPreferredSize(new Dimension(windowW,windowH));
 		setBackground(new Color(47,47,47));
+		
+		addKeyListener(this);
+		setFocusable(true);
+		
+		
 		mazeWidth = 39;
 		mazeHeight = 23;
 		maze = new Tile[mazeWidth][mazeHeight];
+		
+		
+		start = random.nextInt(mazeHeight);
 		buildMaze();
+		
+		playerX = maze[0][start].getX() + 3;
+		playerY = maze[0][start].getY() + 3;
+		
+		
+		velocityX = 0;
+		velocityY = 0;
 		
 		gameLoop = new Timer(1,this);
 		gameLoop.start();
@@ -52,10 +77,25 @@ public class MazeRunner extends JPanel implements ActionListener, KeyListener
 				maze[i][j] = new Tile(i*tileSize +100,j*tileSize+100);
 		
 		//assign start of maze on left side of grid
-		maze[0][random.nextInt(mazeHeight)].setStart(true);
+
+		maze[0][start].setStart(true);
+		
+		//set up player cords
+		
 		
 		//assign end of maze on right side of grid
-		maze[38][random.nextInt(mazeHeight)].setEnd(true);
+		end = random.nextInt(mazeHeight);
+		maze[38][end].setEnd(true);
+		
+		
+		
+		DFS(random.nextInt(mazeWidth),random.nextInt(mazeHeight));
+	}
+	
+	public void move() 
+	{
+		playerX += velocityX;
+		playerY += velocityY;
 	}
 
 	
@@ -100,6 +140,10 @@ public class MazeRunner extends JPanel implements ActionListener, KeyListener
 				
 			}
 		}
+		
+		//draw player
+		g.setColor(Color.BLUE);
+		g.fillOval(playerX, playerY, 20,20);
 			
 		
 	}
@@ -108,7 +152,67 @@ public class MazeRunner extends JPanel implements ActionListener, KeyListener
 	//                             Randomized Depth-First Search
 	/////////////////////////////////////////////////////////////////////////////////////////
 	
-	public boolean checkIfallTilesVisited()
+	public void DFS(int x, int y)
+	{
+		Tile currentTile = maze[x][y];
+		currentTile.setVisited(true);
+		
+		int[] directions = {0,1,2,3};
+		for(int i = 3; i > 0; i--)
+		{
+			int j = random.nextInt(i+1);
+			
+			int temp = directions[i];
+			directions[i] = directions[j];
+			directions[j] = temp;
+		}
+		for(int direction: directions)
+		{
+			int newX = x;
+			int newY = y;
+			
+			if(direction == 0) newX = x-1;
+			if(direction == 1) newY = y -1;
+			if(direction == 2) newX = x+1;
+			if(direction == 3) newY = y+1;
+			
+			if(newX < 0 || newY < 0 || newX > mazeWidth-1 || newY > mazeHeight-1) continue;
+			
+			Tile neighborTile = maze[newX][newY];
+			
+			if(neighborTile.getVisited()) continue;
+			
+			if(direction == 0)
+			{
+				currentTile.setLeft(false);
+				neighborTile.setRight(false);
+			}
+			
+			if(direction == 1)
+			{
+				currentTile.setTop(false);
+				neighborTile.setBottom(false);
+			}
+			
+			if(direction == 2)
+			{
+				currentTile.setRight(false);
+				neighborTile.setLeft(false);
+			}
+			
+			if(direction == 3)
+			{
+				currentTile.setBottom(false);
+				neighborTile.setTop(false);
+			}
+			
+			DFS(newX,newY);
+		}
+		
+		
+	}
+
+	public boolean checkIfAllTilesVisited()
 	{
 		for(int i = 0; i < mazeWidth; i ++) {
 			for(int j = 0; j < mazeHeight; j++) {
@@ -116,20 +220,19 @@ public class MazeRunner extends JPanel implements ActionListener, KeyListener
 				if(!tile.visited) return false;
 			}
 		}
-		System.out.println("Done");
+		//System.out.println("Done");
 		return true;
 	}
 	
-	
-	
 	/////////////////////////////////////////////////////////////////////////////////////////
-
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(!checkIfallTilesVisited())
-			//randomRemoveLines();
-		repaint();
+		//if(checkIfAllTilesVisited()) 
+			move();
+			repaint();
+		
+		
 	}
 
 	@Override
@@ -139,14 +242,51 @@ public class MazeRunner extends JPanel implements ActionListener, KeyListener
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+	public void keyPressed(KeyEvent e) 
+	{
+		switch (e.getKeyCode())
+		{
+		case KeyEvent.VK_W,KeyEvent.VK_UP:
+			velocityX = 0;
+			velocityY = -2;
+			break;
+		case KeyEvent.VK_S,KeyEvent.VK_DOWN:
+			velocityX = 0;
+			velocityY = 2;
+			break;
+		case KeyEvent.VK_D,KeyEvent.VK_RIGHT:
+			velocityX = 2;
+			velocityY = 0;
+			break;
+		case KeyEvent.VK_A,KeyEvent.VK_LEFT:
+			velocityX = -2;
+			velocityY = 0;
+			break;
+		}
 		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
+		switch (e.getKeyCode())
+		{
+		case KeyEvent.VK_W,KeyEvent.VK_UP:
+			velocityX = 0;
+			velocityY = 0;
+			break;
+		case KeyEvent.VK_S,KeyEvent.VK_DOWN:
+			velocityX = 0;
+			velocityY = 0;
+			break;
+		case KeyEvent.VK_D,KeyEvent.VK_RIGHT:
+			velocityX = 0;
+			velocityY = 0;
+			break;
+		case KeyEvent.VK_A,KeyEvent.VK_LEFT:
+			velocityX = 0;
+			velocityY = 0;
+			break;
+		}
 		
 	}
 
